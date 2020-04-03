5735ed26d210f86ea075631714cf037d33774f19
ZakMos <z.zza@hotmail.com>
2020-04-03 13:21:45 +0200

    added hash bcrypt password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render("login"));
@@ -27,18 +31,51 @@ router.post('/register', (req, res) => {
     if(password.length < 6) {
         errors.push({ msg: 'Password should be at least 6 characters'})
     }
-
-    if(errors.length > 0) {
+    if (errors.length > 0) {
         res.render('register', {
-            errors,
-            name,
-            email,
-            password,
-            password2
+          errors,
+          name,
+          email,
+          password,
+          password2
         });
-    } else {
-        res.send('pass');
+      } else {
+        // Validation passed 
+        User.findOne({ email: email })
+       .then(user => {
+          if (user) {
+            // User exists
+            errors.push({ msg: 'Email already exists' });
+            res.render('register', {
+              errors,
+              name,
+              email,
+              password,
+              password2
+            });
+          } else  {
+            const newUser = new User({
+              name,
+              email,
+              password
+            });
+                //Hash Password
+                bcrypt.genSalt(10, (err, salt) => 
+                  bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    //Set Password to hashed
+                    newUser.password = hash;
+                    // Save User
+                    newUser.save()
+                    .then(user => {
+                      res.redirect('/users/login');
+                    })
+                    .catch(err => console.log(err));
+                }))           
+              }
+          });
     }
-})
+
+});
 
 module.exports = router;
