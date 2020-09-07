5aab5bbd55a0541624711fae11cfbce7edc1e792
ZakMos <z.zza@hotmail.com>
2020-09-07 06:41:09 +0200

    fix bugs

--- config/passport.js
+++ config/passport.js
@@ -1,27 +1,21 @@
-// @ts-ignore
+// @ts-nocheck
 const Localstrategy = require('passport-local').Strategy;
-// @ts-ignore
-const mongoose = require('mongoose');
-// @ts-ignore
 const bcrypt = require('bcryptjs');
 
 // Load User Model
-// @ts-ignore
 const User = require('../models/User');
-// @ts-ignore
+
 module.exports = function(passport) {
     passport.use(
-        // @ts-ignore
         new Localstrategy({ usernameField: 'email' }, (email, password, done) => {
             // Match User
             User.findOne({ email: email })
-            // @ts-ignore
             .then(user => {
                 if(!user) {
                     return done(null, false, { message: 'That email is not registered'});
                 }
+
                 // Match Password
-                // @ts-ignore
                 bcrypt.compare(password, user.password, (err, isMatch) => {
                     if(err) throw err;
                     if(isMatch) {
@@ -31,21 +25,17 @@ module.exports = function(passport) {
                     }
                 });
             })
-            // @ts-ignore
             .catch(err => console.log(err))
         })
     );
     
-    // @ts-ignore
     passport.serializeUser((user, done) => {
         done(null, user.id);
     });
 
-    // @ts-ignore
     passport.deserializeUser((id, done) => {
-        // @ts-ignore
         User.findById(id, (err, user) => {
             done(err, user);
         });
     });
-}
+};
